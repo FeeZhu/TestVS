c29bff8427c39fdaf70c382ebffc5e08d759e8e8
unknown <>
2017-09-26 10:28:49 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+// ConsoleApplication1.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int elementry(int);
+
+int main()
+{
+	for (auto &x : { 10,21,32 })
+	{
+		cout <<elementry(x) << '\n';
+	}
+	return 0;
+}
+
+int elementry(int x)
+{
+	return x*x;
+}
